60d0c12e9836d1b42927668e9900cd553b7e5b9f
Richard Green <rg@ie.com.au>
2016-04-04 10:15:27 +1000

    Added basic homepage and category list example

--- src/cms/homeContainer.js
+++ src/cms/homeContainer.js
@@ -2,20 +2,11 @@ import React from 'react';
 import { Route, Link } from 'react-router';
 
 const welcomeStructure = {
-  children: [
-    '<h1>Shop the full line of Vionic products.</h1>',
-    '<li>Browse all Men\'s and Women\'s styles.</li>',
-    '<li>See product details and reviews.</li>',
-    '<li>Have items delivered to this store or directly to your home.</li>',
-    'start',
-    {
-      className: 'techLink',
-      children: [
-        <Link to="/interact/cms/technology">About our Orthaheel technology ></Link>
-      ]
-    }
-
-  ]
+    children: [
+        '<h1>Hello</h1>',
+        '<p>This is the welcome screen of the FoyerLive example theme.</p>',
+        'start',
+    ]
 };
 
 export default welcomeStructure
