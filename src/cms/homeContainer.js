61375d32576a1cf4e499a9a2c2fd0d0f80cb54c5
James Turle <james@foyerlive.com>
2017-06-02 15:30:10 -0400

    Removing embedded collection

--- src/cms/homeContainer.js
+++ src/cms/homeContainer.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Route, Link} from 'react-router';
 import FLCollection from './util/embeddedCollection';
 
-let collectionCode = 'deck57a244297660e0-59820926';
+let collectionCode = 'deck57682c995dffd0-30338856';
 if (__ISPROD__)
   collectionCode = 'deck592dd90a977bc9-77341950';
 
 const welcomeStructure = {
   children: [
-    <FLCollection code={collectionCode} api={retrieveDataManager()} style={{height: 'calc(100vw * 0.5625)'}}/>,
+    //<FLCollection code={collectionCode} api={retrieveDataManager()} style={{height: 'calc(100vw * 0.5625)'}}/>,
     {
       className: 'panel',
       children: [
